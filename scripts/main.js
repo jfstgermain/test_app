0e9ec721a4ca45add24666ac9154e03c2ae12f59
Jean-François St-Germain <jfstgermain@gmail.com>
2012-10-31 17:17:06 -0400

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,17 @@
 require.config({    
 	paths: {
-	    sammy: "/scripts/libs/sammy-latest.min"
-	}/*,
+	    sammy: "libs/sammy-latest.min",
+        logger: "libs/logger"
+	},
 	shim: {
-		"sammy": {
-            deps: ["jquery"],
-            exports: "Sammy"    
-		},
-        "jquery":{
-            exports: "$"    
-    	},
-        "routes":["sammy"]
-	}*/
+        "logger":["jquery"]
+	}
 });
 
-require(["sammy"], function(Sammy) {	
+require(["sammy", "logger"], function(Sammy) {	
+    console.log("js");
 	//the jquery.alpha.js and jquery.beta.js plugins have been loaded.
     $(function() {
-        console.log(Sammy);
         var app = Sammy(function() {
 
             this.get('#/', function() {
@@ -26,5 +20,6 @@ require(["sammy"], function(Sammy) {
         });
         
         app.run();
+        log('inside coolFunc', this, null);
     });
 });
